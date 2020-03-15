a78196cca553326f034b4367abd4cd1542e78f2e
ntno <organick.natan@gmail.com>
2020-03-14 23:03:15 -0400

    added offset param

--- get-photos.sh
+++ get-photos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl api.tumblr.com/v2/blog/${BLOG_NAME}/posts/photo?api_key=${TUMBLR_OAUTH_CONSUMER_KEY} > temp.json
+curl "api.tumblr.com/v2/blog/${BLOG_NAME}/posts/photo?api_key=${TUMBLR_OAUTH_CONSUMER_KEY}&limit=20&offset=$2" > temp.json
 cat temp.json | jq . > $1
 rm temp.json
